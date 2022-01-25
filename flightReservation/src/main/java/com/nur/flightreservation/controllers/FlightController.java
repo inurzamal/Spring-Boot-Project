414e534fe69d379bf5af676faf35d2629a4ad565
Nurzamal Islam <n.zaman98@gmail.com>
2022-01-25 17:57:44 +0530

    working on search flight

---
+++ flightReservation/src/main/java/com/nur/flightreservation/controllers/FlightController.java
@@ -0,0 +1,34 @@
+package com.nur.flightreservation.controllers;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nur.flightreservation.entities.Flight;
+import com.nur.flightreservation.repos.FlightRepository;
+
+@Controller
+public class FlightController {
+	
+	@Autowired
+	FlightRepository flightRepository;
+	
+	@GetMapping("/searchflight")
+	public String searchflightHandler(@RequestParam("from") String from, @RequestParam("to") String to, 
+			@RequestParam("departureDate") @DateTimeFormat(pattern="dd/mm/yyyy") Date departureDate, Model model) {
+		
+		List<Flight> flights = flightRepository.findFlights(from, to, departureDate);
+		
+		model.addAttribute("flights", flights);
+		
+		return "displayFlights";
+		
+	}
+
+}
--- flightReservation/src/main/java/com/nur/flightreservation/controllers/UserController.java
+++ flightReservation/src/main/java/com/nur/flightreservation/controllers/UserController.java
@@ -2,9 +2,11 @@ package com.nur.flightreservation.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nur.flightreservation.entities.User;
 import com.nur.flightreservation.repos.UserRepository;
@@ -20,11 +22,27 @@ public class UserController {
 		return "login/registerUser";
 	}
 	
-	@PostMapping("/registration")
+	@PostMapping("/login1")
 	public String registrationHandler(@ModelAttribute("user") User user) {
 		
 		userRepository.save(user);
 		return "login/login";
 	}
-
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login/login";
+	}
+	
+	@PostMapping("/findflights")
+	public String loginHandler(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+		User user = userRepository.findByEmail(email);
+		if(user.getPassword().equals(password)) {
+			return "findFlights";
+		}else {
+			model.addAttribute("msg", "Inavalid Email or Password");
+		}
+		return "login/login";
+	}
+	
 }
