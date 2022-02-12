b74194559426dd13ae68a6f15e2721fee3bc8d3c
unknown <n.zaman98@gmail.com>
2022-02-12 23:28:38 +0530

    Flight Reservation App

--- flightReservation/src/main/java/com/nur/flightreservation/services/ReservationServiceImpl.java
+++ flightReservation/src/main/java/com/nur/flightreservation/services/ReservationServiceImpl.java
@@ -3,6 +3,7 @@ package com.nur.flightreservation.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.nur.flightreservation.dto.ReservationRequest;
@@ -18,6 +19,9 @@ import com.nur.flightreservation.util.PDFGenerator;
 @Service
 public class ReservationServiceImpl implements ReservationService {
 	
+	@Value("${com.nur.flightreservation.itinerary.dirpath}")
+	private String ITINERARY_DIR;
+
 	@Autowired
 	FlightRepository flightRepository;
 	
@@ -70,7 +74,7 @@ public class ReservationServiceImpl implements ReservationService {
 		LOGGER.info("Saving the reservation "+reservation);
 		Reservation savedReservation = reservationRepository.save(reservation);
 		
-		String filePath = "C:\\Users\\User\\Documents\\reservations\\reservation"+savedReservation.getId()+".pdf";
+		String filePath=ITINERARY_DIR+savedReservation.getId()+".pdf";
 
 		
 		LOGGER.info("Generating Ticket - filePath: "+filePath);
--- flightReservation/src/main/java/com/nur/flightreservation/util/EmailUtil.java
+++ flightReservation/src/main/java/com/nur/flightreservation/util/EmailUtil.java
@@ -8,6 +8,7 @@ import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailUtil {
 	
+	@Value("${com.nur.flightreservation.itinerary.email.subject}")
+	private String EMAIL_SUBJECT;
+	
+	@Value("${com.nur.flightreservation.itinerary.email.body}")
+	private String EMAIL_BODY;
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmailUtil.class);
 	
 	@Autowired
@@ -29,8 +36,8 @@ public class EmailUtil {
 		try {
 			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
 			messageHelper.setTo(toAddress);
-			messageHelper.setSubject("Itinerary for your Flight");
-			messageHelper.setText("Please find your Itinerary attached");
+			messageHelper.setSubject(EMAIL_SUBJECT);
+			messageHelper.setText(EMAIL_BODY);
 			messageHelper.addAttachment("Itinerary", new File(filePath));
 			sender.send(message);
 		} catch (MessagingException e) {
